257081762d94b965f5e5c64714afe456e3898f49
acgerstein <gerstein@zoology.ubc.ca>
2015-01-20 18:45:14 -0600

    hard coded number of maximum digits in filename (30)

--- R/readInExistingIJ.R
+++ R/readInExistingIJ.R
@@ -3,10 +3,11 @@
 #' @description This function can be used to read in existing imageJ analyses following either \code{\link{runIJ}} or \code{\link{runIJManual}}. Running this function will prompt the user to select the main project folder and to select the directory that contains 
 
 #' @param projectName the name to be used for project. This name should be short, and can be different than what was used originally for the imageJ analysis step.
+#' @param newList dummy variable
 
 #' @export
 
-readInExistingIJ <- function(projectName, newList = list(), numDig = 30) {
+readInExistingIJ <- function(projectName, newList = list()) {
 	curDir <- getwd()
 	projectFolder <- tcltk::tk_choose.dir(caption = "Select main project folder") 
 	directoryPath <- tcltk::tk_choose.dir(default = "", caption = "Select directory with ImageJ output")
@@ -23,7 +24,7 @@ readInExistingIJ <- function(projectName, newList = list(), numDig = 30) {
 		else {
 			allLines <-  aggregate(.load.data(dir()[i])$x,  .load.data(dir()[i])["distance"], mean)
 			newList[[length(newList)+1L]] <-  data.frame(distance = allLines[,1]*40/length(allLines[,1]), x= allLines[,2])
-			temp <- paste(substr(basename(dir()[i]),1,numDig), "", sep="")
+			temp <- paste(substr(basename(dir()[i]),1, 30), "", sep="")
 			names[i] <- strsplit(temp,".txt")[[1]][1]
 			getData(i+1, newList, names)
 		}
