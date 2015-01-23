374ef86379efd502d36e33cc2b501683483eb32c
acgerstein <gerstein@zoology.ubc.ca>
2015-01-23 12:03:44 -0600

    plot fiddling

--- R/maxLik.R
+++ R/maxLik.R
@@ -343,7 +343,7 @@ maxLik <- function(projectName, clearHalo, diskDiam = 6, maxDist=30, ymax=125, x
 	}
 	
 	mtext("Distance (mm)", outer=TRUE, side=1, line=2, cex=1.2)
-	mtext("Pixel intensity", outer=TRUE, side=2, line=1.5, cex=1.2)
+	mtext("Pixel intensity", outer=TRUE, side=2, line=2, cex=1.2)
 
 	if(savePDF){
 		dev.off()	
