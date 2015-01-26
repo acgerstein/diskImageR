74b34275ac3eae357cb40081ea3d705aa11cbb8b
acgerstein <gerstein@zoology.ubc.ca>
2015-01-26 11:31:34 -0600

    adding parameter documentation

--- R/plottingFunc.R
+++ R/plottingFunc.R
@@ -3,16 +3,16 @@
 #' @description This function creates a pdf figure of plots showing the results of the imageJ analysis for resistance (ZOI) and tolerance (AUC).
 
 #' @inheritParams plotRaw
-#' @param ZOImin
-#' @param tolMax
-#' @param slopeMax
-#' @param xlabels
-#' @param xlabels
-#' @param xlabAngle
-#' @param order
-#' @param orderFactor
+#' @param ZOImin minimum distance from the disk for resistance plot (minimum y axis value), default = 30.
+#' @param tolMax maximum y axis value for tolerance plot. Note tolerance is coverted to a perent, default = 100.
+#' @param slopeMax maximum y value for sensitivity plot, default = 200
+#' @param xlabels either a vector containing the desired x-axis labels, or a single value indicating the column name that contains the values to use (likely either the 'line' column or one of the type columns), default = "line"
+#' @param xlabAngle indicates whether to print the x axis labels on a angle, if a number is provided this will be the angle used. The defauilt is not to plot on an angle, default = NA
+#' @param order can be either "factor" or "custom". If custom, supply a numberial vector the same length as the dataframe to indicate the desired order. If factor, supply the column name in \code{ordeFactor} to be used to factor. 
+#' @param orderFactor if \code{order = "factor"} supply the column name to be used to factor. 
+#' @param barplot in singleParamPlot, whether to plot a barplot (barplot = TRUE) or dotplot (barplot = FALSE), default = FALSE. In double and triple parameter plots, whether to plot tolerance as a barplot (barplot = TRUE) or dotplot (barplot = FALSE), default = TRUE.
 
-#' @details
+#' @details Basic parameter plotting functions. Supports single (any), double (ZOI and fAUC), and triple (ZOI, fAUC , slope) parameter plots. Input can be the dataframe from either \link{code{createDataframe}} \code{type="df"} or from \link{code{aggregateData}} \code{type=="ag"}. Single parameter plot can be either a barplot \code{barplot = TRUE} or a dotplot \code{barplot=FALSE}. In the two and three parameter plots the default is to plot tolerance as a barplot and ZOI and slope as a dotplot, tolerance can also be plotted as a dotplot with \code{barplot=FALSE} though there is currently not support to plot either ZOI or slope as a barplot in this framework. 
 
 #' @return Either a pdf figure figure saved to the 'figures' directory or a figure on screen
 
@@ -20,8 +20,12 @@
 
 #' @author Aleeza c. Gerstein
 
-twoParamPlot <- function(projectName, type, ZOI = "ZOI20", AUC = "fAUC20", ZOImin = 30, tolMax = 100, slopeMax = 200, width = 6, height = 4, xlabels="line", xlabAngle=NA, order=NA, orderFactor = "line", overwrite=TRUE, savePDF= TRUE, popUp = TRUE){
-	
+twoParamPlot <- function(projectName, type, ZOI = "ZOI20", AUC = "fAUC20", ZOImin = 30, tolMax = 100, slopeMax = 200, width = 6, height = 4, xlabels="line", xlabAngle=NA, order=NA, orderFactor = "line", overwrite=TRUE, savePDF= TRUE, popUp = TRUE, barplot=TRUE){
+	if(!(hasArg(type))){
+		cont <- readline(paste("Please select whether dataframe is from 'createDataframe' (df) or `aggregateData (ag) ", sep=""))
+		type <- cont
+	}
+
 	dir.create(paste("figures/", projectName,  sep=""), showWarnings = FALSE)
 	t <- file.path("figures", projectName,  paste(projectName, "_ZOI-fAUC.pdf", sep=""))
 	if (!overwrite){
@@ -43,7 +47,7 @@ twoParamPlot <- function(projectName, type, ZOI = "ZOI20", AUC = "fAUC20", ZOImi
 		if(order=="factor"){
 			ordData <- arrange(data, data[, orderFactor])
 			}
-		if(order=="custom"){
+		if(!order=="factor"){
 			ordData <- data %>%
 						 		mutate(order) %>%
 									arrange(order)			
@@ -66,24 +70,34 @@ twoParamPlot <- function(projectName, type, ZOI = "ZOI20", AUC = "fAUC20", ZOImi
 	if(savePDF){
 		 pdf(t, width=width, height=height)
 		}	
+	
 	par(mfrow=c(2, 1), oma=c(4, 4, 1, 1), mar=c(1, 1, 1, 1))
+	
 	if(type=="ag"){
 		plot(mp[1,], ordData[, ZOI], ylim=c(ZOImin, 0), yaxt="n", xaxt="n", yaxs="i", xaxs="i", pch=19, xlab="", ylab="", col=grey(0.3), 	xlim=c(0, max(mp)+1), cex=1.4)
 		arrows(mp[1,], ordData[, ZOI]-ordData[, paste(var, ".", ZOI, sep="")], mp[1,], ordData[, ZOI]+ordData[,paste(var, ".", ZOI, sep="")], length=0)
 	axis(1, at=mp[1,], labels=FALSE)
 	}
+	
 	if(type=="df"){
 		plot(as.numeric(as.factor(ordData[, orderFactor])), ordData[, ZOI], ylim=c(ZOImin, 0), yaxt="n", xaxt="n", yaxs="i", xaxs="i", pch=19, xlab="", ylab="", col=grey(0.3), cex=1.4, xlim=c(0.5, length(xlabels)+0.5))
 	axis(1, at=as.numeric(as.factor(unique(ordData[, orderFactor]))), labels=FALSE)
 	}
+	
 	axis(2, las=2, cex.axis=0.8)
 	mtext("Distance\n from disk (mm)", side=2, line=2.5, cex=0.8)
 	mtext(expression(paste(bold(A), " Resistance", sep="")), side=3, adj=0.01)
 	
-	if(type=="ag"){	
+	if(type=="ag"){
+		if(barplot == TRUE){	
 		mp <- barplot(t(tols*100), ann=FALSE, beside=TRUE, yaxs="i", xaxs="i", ylim=c(0, tolMax), xaxt="n", yaxt="n", xlab="", ylab="", xlim=c(0, max(mp)+1))
 		box()
-		 arrows(mp[1,], ordData[,AUC]*100-ordData[,paste(var, ".", AUC, sep="")]*100, mp[1,], ordData[,AUC]*100+ ordData[,paste(var, ".", AUC, sep="")]*100, length=0)
+		}
+	else{
+		plot(as.numeric(as.factor(ordData[, orderFactor])), ordData[, fAUC], ylim=c(0, tolMax), yaxt="n", xaxt="n", yaxs="i", xaxs="i", pch=19, xlab="", ylab="", col=grey(0.3), cex=1.4, xlim=c(0.5, length(xlabels)+0.5))
+	axis(1, at=as.numeric(as.factor(unique(ordData[, orderFactor]))), labels=FALSE)
+	}
+	 arrows(mp[1,], ordData[,AUC]*100-ordData[,paste(var, ".", AUC, sep="")]*100, mp[1,], ordData[,AUC]*100+ ordData[,paste(var, ".", AUC, sep="")]*100, length=0)
 		if(is.na(xlabAngle)) 	axis(1, at=mp[1,], labels=xlabels)
 		else{
 			axis(1, at=mp[1,], labels=FALSE)
@@ -224,7 +238,7 @@ threeParamPlot <- function(projectName, type, ZOI = "ZOI20", AUC = "fAUC20", ZOI
 
 }
 
-oneParamPlot <- function(projectName, type, param  = "ZOI20", ymin = 0, ymax = 100, width = 6, height = 4, xlabels="line", xlabAngle=NA, order=NA, orderFactor = "line", overwrite=TRUE, savePDF= TRUE, popUp = TRUE, hist = TRUE){
+oneParamPlot <- function(projectName, type, param  = "ZOI20", ymin = 0, ymax = 100, width = 6, height = 4, xlabels="line", xlabAngle=NA, order=NA, orderFactor = "line", overwrite=TRUE, savePDF= TRUE, popUp = TRUE, barplot = TRUE){
 	
 	dir.create(paste("figures/", projectName,  sep=""), showWarnings = FALSE)
 	t <- file.path("figures", projectName,  paste(projectName, "_ZOI-fAUC.pdf", sep=""))
@@ -282,7 +296,7 @@ oneParamPlot <- function(projectName, type, param  = "ZOI20", ymin = 0, ymax = 1
 	par(mfrow=c(2, 1), oma=c(4, 4, 1, 1), mar=c(1, 1, 1, 1))
 
 	if(type=="ag"){
-		if(hist!=TRUE){
+		if(barplot!=TRUE){
 			plot(mp[1,], ordData[, param], ylim=yrange, yaxt="n", xaxt="n", yaxs="i", xaxs="i", pch=19, xlab="", ylab="", col=grey(0.3), 	xlim=c(0, max(mp)+1), cex=1.4)
 			arrows(mp[1,], ordData[, ZOI]-ordData[, paste(var, ".", ZOI, sep="")], mp[1,], ordData[, ZOI]+ordData[,paste(var, ".", ZOI, sep="")], length=0)
 		axis(1, at=mp[1,], labels=FALSE)
