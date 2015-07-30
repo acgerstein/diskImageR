b140208a326a21406b617144190c6a5d96e9c1ba
acgerstein <acgerstein@gmail.com>
2015-07-30 10:43:49 -0500

    add back doc, for now

---
+++ doc/DiskAssay.R
@@ -0,0 +1,46 @@
+## ----setup, message=FALSE, echo=FALSE------------------------------------
+library(knitr)
+# This is necessary to direct knitr to find the 
+# 'data', and other directories that contain
+# files needed to execute this document
+# thanks to http://stackoverflow.com/a/24585750/1036500
+# opts_knit$set(root.dir=normalizePath('../'))
+# opts_chunk$set(fig.path = "../figures/")
+
+## ----, echo = FALSE------------------------------------------------------
+knitr::opts_chunk$set(collapse = TRUE, comment = "#>")
+
+## ----, echo=FALSE--------------------------------------------------------
+library("diskImageR")
+
+## ------------------------------------------------------------------------
+IJMacro("vignette", projectDir= getwd(), pictureDir = file.path(getwd(), "pictures", ""))
+
+## ----, fig.width=6, fig.height=4-----------------------------------------
+plotRaw("vignette", popUp = FALSE, savePDF = FALSE)
+
+## ----,  fig.width=5, fig.height=4----------------------------------------
+maxLik("vignette", clearHalo=1, ZOI="all", needML=FALSE, popUp = FALSE, savePDF =FALSE, AUC=20)
+
+## ------------------------------------------------------------------------
+createDataframe("vignette", clearHalo = 1, typeName="temp")
+vignette.df
+
+## ------------------------------------------------------------------------
+addType("vignette", typeName="rep")
+vignette.df
+
+## ------------------------------------------------------------------------
+manyReps.df <- read.csv(file.path(getwd(), "data", "manyReps_df.csv"))
+head(manyReps.df)
+
+## ------------------------------------------------------------------------
+aggregateData("manyReps", replicate=c("line", "type"), varFunc="se")
+manyReps.ag
+
+## ----, fig.width=5, fig.height=4-----------------------------------------
+twoParamPlot("manyReps", type= "ag", popUp = TRUE, savePDF =FALSE, xlabAngle = -45, order = c(1, 8, 2, 9, 3, 10, 4, 11, 5, 12, 6, 13, 7, 14), xlabels =paste(rep(manyReps.ag$line[1:7], each=2), rep(c("A", "B"), 7), sep="-"))
+
+## ----, fig.width=5, fig.height=4-----------------------------------------
+twoParamTwoGroupPlot("manyReps", type="df", popUp = TRUE, savePDF =FALSE, xlabAngle = -45)
+
