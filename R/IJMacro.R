b73baf7e475cf3f451e2b7d76b9d44e702c24f82
acgerstein <acgerstein@gmail.com>
2016-01-06 10:35:52 -0600

    pictureDir -> photoDir; cleanup

--- R/IJMacro.R
+++ R/IJMacro.R
@@ -4,13 +4,13 @@
 
 #' @param projectName the short name you want use for the project.
 #' @param projectDir the path to the project directory where all analyses will be saved. If using tcltk interface to select directories leave as is (default=NA)
-#' @param pictureDir the path to the directory where the pictures are to be analyzed. If using tcltk interface to select directories leave as is (default=NA)
+#' @param photoDir the path to the directory where the photographs are to be analyzed. If using tcltk interface to select directories leave as is (default=NA)
 #' @param diskDiam the diameter of the diffusion disk in mm, defaults to 6.
 #' @param imageJLoc the absolute path to imageJ on your computer. Current options are those standard for a mac: \code{imageJLoc} = "default" when imageJ is located at /Applications/ImageJ/ImageJ.app/Contents/MacOS/JavaApplicationStub; \code{imageJLoc} = "loc2" for path /Applications/ImageJ.app/Contents/MacOS/JavaApplicationStub; \code{imageJ} will also accept any absolute path. If you are on a windows machine please save imageJ to the default location when installing (Program Files/ImageJ).
 #' @param manual whether to supply path names manually (default = TRUE) or using tcltk interface, i.e., pop-up boxes (manual = FALSE).
 
 
-#' @details Each photograph in the directory specified by \code{pictureDir} is input into ImageJ, where the built-in 'find particles' macro is used to find the center of a drug diffusion disk of the size specified by \code{diskDiam}. Lines are drawn every 5 degrees out from the center of the disk, and the pixel intensity, which corresponds to cell density, is measured using the 'plot-profile' macro along each line. The results from all lines are saved into the "imageJ-out" directory in the specified \code{projectDir}. The average pixel intensity is then determined across all 72 lines at each distance and saved to \code{projectName}. \cr Note that the photograph names can be fairly important downstream and should follow a fairly strict convention to be able to take advantage of some of the built-in functions. Photographs should be named "line_factor1_factor2_factor3_...".
+#' @details Each photograph in the directory specified by \code{photoDir} is input into ImageJ, where the built-in 'find particles' macro is used to find the center of a drug diffusion disk of the size specified by \code{diskDiam}. Lines are drawn every 5 degrees out from the center of the disk, and the pixel intensity, which corresponds to cell density, is measured using the 'plot-profile' macro along each line. The results from all lines are saved into the "imageJ-out" directory in the specified \code{projectDir}. The average pixel intensity is then determined across all 72 lines at each distance and saved to \code{projectName}. \cr Note that the photograph names can be fairly important downstream and should follow a fairly strict convention to be able to take advantage of some of the built-in functions. Photographs should be named "line_factor1_factor2_factor3_...".
 
 #' @return A .csv file is saved to the directory "imageJ-out" in the directory specified by \code{projectDir}. The average line for each photograph is saved to the list \code{projectName} in the global environment.
 
@@ -19,28 +19,29 @@
 #'@author Aleeza C. Gerstein
 
 IJMacro <-
-function(projectName, projectDir=NA, pictureDir=NA, imageJLoc="loc2", diskDiam = 6, manual=TRUE){
+function(projectName, projectDir=NA, photoDir=NA, imageJLoc="loc2", diskDiam = 6, manual=TRUE){
 	# if(!is.char(projectName))
 	fileDir <- projectName
 	if(is.na(projectDir)){
 		projectDir <- tcltk::tk_choose.dir(caption = "Select main project directory") 
 	}		
-	if(is.na(pictureDir)){
-		pictureDir <- tcltk::tk_choose.dir(caption = "Select location of photographs")
-		pictureDirOrig <- pictureDir
-		pictureDir <- file.path(pictureDir, "")
-	}
-	setwd(pictureDir)
-	if (projectDir == pictureDirOrig) {
-		cat("The photograph directory can not be used for the main project directory. Please select a different folder for the main project directory.")
-		projectDir <- tcltk::tk_choose.dir(caption = "Select main project directory") 
+	if(is.na(photoDir)){
+		photoDir <- tcltk::tk_choose.dir(caption = "Select location of photographs")
+		photoDirOrig <- photoDir
+		photoDir <- file.path(photoDir, "")
+		if (projectDir == photoDirOrig) {
+			cat("The photograph directory can not be used for the main project directory. Please select a different folder for the main project directory.")
+			projectDir <- tcltk::tk_choose.dir(caption = "Select main project directory") 
 		}
+
+	}
+	setwd(photoDir)
 	if (TRUE %in% file.info(dir())[,2]) {
 		stop("There is a folder located in your photograph directory. Please remove before continuing.")
 		}
 	dir.create(file.path(projectDir, "imageJ_out"), showWarnings=FALSE)
 	outputDir <- file.path(projectDir, "imageJ_out", fileDir, "")
-	IJarguments <- paste(pictureDir, outputDir, diskDiam, sep="*")	
+	IJarguments <- paste(photoDir, outputDir, diskDiam, sep="*")	
 
 	if(length(dir(outputDir)) > 0){
 		cont <- readline(paste("Output files exist in directory ", outputDir, "\nOverwrite? (y/n) ", sep=""))
@@ -60,7 +61,7 @@ function(projectName, projectDir=NA, pictureDir=NA, imageJLoc="loc2", diskDiam =
 	
 	script <- file.path(.libPaths(), "diskImageR", "IJ_diskImageR.ijm")[1]			
 	if(.Platform$OS.type=="windows"){
-		IJarguments <- paste(paste(pictureDir,  "", sep="\\"), paste(outputDir, "", sep="\\"), diskDiam, sep="*")		
+		IJarguments <- paste(paste(photoDir,  "", sep="\\"), paste(outputDir, "", sep="\\"), diskDiam, sep="*")		
 		script <- gsub("Program Files", "progra~1", script)
 	  cmd <- "C:\\progra~1\\ImageJ\\ImageJ.exe"
 		# cmd <- '"C:\\Program Files (x86)\\ImageJ\\ImageJ.exe"'
@@ -80,10 +81,10 @@ function(projectName, projectDir=NA, pictureDir=NA, imageJLoc="loc2", diskDiam =
 		system(call)
 	}
 	
-	cat(paste("\nOutput of imageJ analyses saved in directory: ", outputDir, "\n", sep=""))
-	cat(paste("\nElements in dataframe ", projectName, ": \n", sep=""))	
+	cat(paste("\nOutput of imageJ analyses saved in directory: \n", outputDir, "\n", sep=""))
+	cat(paste("\nElements in list '", projectName, "': \n", sep=""))	
 	temp <- .ReadIn_DirCreate(projectDir, outputDir, projectName)
-	if(!length(dir(pictureDir)) == length(temp)){
+	if(!length(dir(photoDir)) == length(temp)){
 		stop("Mismatch between the number of files in the photograph directory and the number of images analyzed. This likely indicates a non-photograph file is located in this directory. Please remove and rerun before continuing.")
 		}		
 	cat("\a")
