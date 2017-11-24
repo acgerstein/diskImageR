d12aba3a1de04196d79d73e064b2addc6e74fb56
Aleeza Gerstein <acgerstein@aleeza.gateway.2wire.net>
2017-11-24 09:20:10 -0600

    put drug name to coordinates (by Y not X, duh)

--- R/IJMacro16.R
+++ R/IJMacro16.R
@@ -226,8 +226,8 @@ function(filename) {
      fileFolder <- projectName
     mapDir <- file.path(getwd(), "disk_coordinates", fileFolder)
      map <- read.csv(file.path(mapDir, paste0(m, "_ResultsTable.txt")), sep="\t")
-     map$XYpos <- c(order(map[1:4, "Y" ]), order(map[5:8, "Y"])+4, order(map[9:12, "Y" ])+8, order(map[13:16, "Y" ])+12)
-     map$drugs <- drugs[map$XYpos]
+		 map$XYpos <- order(map[, "Y" ])
+		 map$drugs <- drugs[map$XYpos]
      write.table(map, file.path(mapDir, paste0(m, "_ResultsTable.txt")), row.names=FALSE, sep="\t")
 		 assign(paste(m, "map", sep="."), map, inherits=TRUE)
 		 cat(paste0("\t", paste(m, "map", sep=".")))
