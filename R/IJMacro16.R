ea505808082226afe712bc9035b40d45b84ec7fc
Aleeza Gerstein <acgerstein@aleeza.gateway.2wire.net>
2017-11-24 08:59:10 -0600

    put drug name to coordinates

--- R/IJMacro16.R
+++ R/IJMacro16.R
@@ -225,7 +225,7 @@ function(filename) {
 	 for(m in photoNames){
      fileFolder <- projectName
     mapDir <- file.path(getwd(), "disk_coordinates", fileFolder)
-     map <- read.csv(file.path(mapDir, paste0(i, "_ResultsTable.txt")), sep="\t")
+     map <- read.csv(file.path(mapDir, paste0(m, "_ResultsTable.txt")), sep="\t")
      map$XYpos <- c(order(map[1:4, "X" ]), order(map[5:8, "X"])+4, order(map[9:12, "X" ])+8, order(map[13:16, "X" ])+12)
      map$drugs <- drugs[map$XYpos]
      write.table(map, file.path(mapDir, paste0(,m, "_ResultsTable.txt")), row.names=FALSE, sep="\t")
