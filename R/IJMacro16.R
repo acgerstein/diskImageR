6165d4b18328e642689f2305401fad54b1d46b24
Aleeza Gerstein <acgerstein@aleeza.gateway.2wire.net>
2017-12-04 20:46:13 -0600

    change the data kept

--- R/IJMacro16.R
+++ R/IJMacro16.R
@@ -193,7 +193,7 @@ function(workingDir, folderLoc, experAbbr){
 		  }
 		 names(newd)[3:180] <- paste0("L",2:179)
 
-		  aveSorted <- apply(newd, 1, function(x) mean(sort(x)[(130-numTop):130]))
+		  aveSorted <- apply(newd, 1, function(x) mean(sort(x)[(150-numTop):150]))
 			#the 25 comes from the IJ16 macro
 			newList[[length(newList)+1L]] <-  data.frame(distance = newd$x*30/length(newd$x), x= aveSorted)
 			# temp <- paste(substr(basename(dir()[i]),1,numDig), "", sep="")
