573e38743771c8e2a8f0daaeb31422f32f4d0e4b
Aleeza Gerstein <acgerstein@aleeza.gateway.2wire.net>
2017-12-08 15:08:14 -0600

    small ZOI slope calc

--- R/createDataframe.R
+++ R/createDataframe.R
@@ -40,7 +40,6 @@ if(standType=="one"){
 		}
 	}
 	data <- eval(parse(text=projectName))
-	print("before map")
 	if(needMap){
 		 mapDir <- file.path(getwd(), "disk_coordinates", projectName)
 		 map <- read.csv(file.path(mapDir, paste0(projectName, "_ResultsTable.txt")), sep="\t")
@@ -210,7 +209,6 @@ else{
 	if(standType == "indiv") data[[i]]$x <- data[[i]]$x -min(data[[i]]$x)
 	data[[i]]$distance <- data[[i]]$distance - dotedge
 	maxY <- which(data[[i]][,2] > (ML2[[i]]$par[1]+ML2[[i]]$par[5]))[1]
-	abline(v=data[[i]][maxY, 1])
 	xx <- seq(log(data[[i]]$distance[1]), log(max(data[[i]][,1])), length=200)
 	yy<- .curve(ML[[i]]['par'][1]$par[1], ML[[i]]['par'][1]$par[2], ML[[i]]['par'][1]$par[3],xx)
 	xcross <- exp(ML[[i]]['par'][1]$par[2])
