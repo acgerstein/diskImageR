2cf6eca593791c0a32f0fe691846b7a5dea5a77e
acgerstein <acgerstein@gmail.com>
2018-02-19 15:29:50 -0600

    one ZOI

--- R/createDataframe.R
+++ R/createDataframe.R
@@ -182,15 +182,9 @@ else{
 }
 
 	if(addZOI){
-		df$ZOI20 <- round(df$RAD20*2+diskDiam, 0)
-		df$ZOI50 <- round(df$RAD50*2+diskDiam, 0)
-		df$ZOI80 <- round(df$RAD80*2+diskDiam, 0)
-		df$ZOI20[df$RAD20 ==0] <- 6
-		df$ZOI50[df$RAD50 ==0] <- 6
-		df$ZOI80[df$RAD80 ==0] <- 6
-		df$ZOI20[df$slope < 0 ] <- 6
-		df$ZOI50[df$slope < 0 ] <- 6
-		df$ZOI80[df$slope < 0 ] <- 6
+		df$ZOI <- round(df$RAD50*2+diskDiam, 0)
+		df$ZOI[df$RAD50 ==0] <- 6
+		df$ZOI[df$slope < 0 ] <- 6
 	}
 	df <- df[order(df$photo, df$drug),]
 
