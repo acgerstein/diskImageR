60ef3add6b6809ac03d550ac1c67031891ffce00
acgerstein <acgerstein@gmail.com>
2018-02-24 22:52:59 -0600

    debug

--- R/createDataframe.R
+++ R/createDataframe.R
@@ -441,8 +441,10 @@ if(addSIR){
 }
 
 .findRAD <- function(data, ML, ML2, dotedge, maxDist, i){
+  print(i)
 		startX <- which(data[[i]][,1] > dotedge)[1]
 		stopX <- which(data[[i]][,1] > maxDist - 0.5)[1]
+
 		data[[i]] <- data[[i]][startX:stopX, 1:2]
 		# minD <- min(data[[i]][startX:stopX, "x"])
 		data[[i]]$x <- data[[i]]$x -min(data[[i]]$x[1:20])
