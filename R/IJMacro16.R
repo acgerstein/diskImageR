84b2fe524e0cbc6dd9852f9ce0eaa70a4477d363
acgerstein <acgerstein@gmail.com>
2018-02-28 11:45:01 -0600

    user supplied drugs

--- R/IJMacro16.R
+++ R/IJMacro16.R
@@ -30,7 +30,8 @@ function(projectName, projectDir=NA, photoDir=NA, imageJLoc=NA, diskDiam = 6, dr
 	fileDir <- projectName
 	#get R version and use appropriate tcltk
 	if(is.na(projectDir)){
-	  corDrugs <- readline(paste("Is ", drugs, "the correct order of drugs on your plate? [y/n] ", sep=""))
+	  drugOrder <- paste(drugs, collapse=", ")
+	  cont <- readline(paste("Is ", drugOrder, " the correct order of drugs on your plate? [y/n] ", sep=""))
 		if(cont=="n"){
 			prompt <- "Enter the correct drug names from top left to bottom right (comma-separated list) \n"
 	    x <- readline(prompt)
