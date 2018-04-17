6c542067fd28b8e2289b3a3dc803db9a6e01a46d
acgerstein <acgerstein@gmail.com>
2018-04-17 14:43:05 -0500

    cleanup

--- inst/walkthrough_growInhib.R
+++ inst/walkthrough_growInhib.R
@@ -14,30 +14,18 @@ library(diskImageR)
 ###################################################################################################
 
 #To use a pop-up box interface:
-IJMacro16("mTest1")
+IJMacro("newProject")
 
 #OR To specify the appropriate directories without the pop-up interface:
-IJMacro16("newProject",  "/path/to/projectDir", "/path/to/projectDir/photographs/")
+IJMacro("newProject",  "/path/to/projectDir", "/path/to/projectDir/photographs/")
 
 ###################################################################################################
 #Use maximum likelihood to fit single and double logistic models to the data from each photograph.
 ###################################################################################################
-maxLik("mTest1", maxDist = 25, standType = "indiv", needMap = TRUE, testInhib = TRUE)
+maxLik("newProject", maxDist = 25, standType = "indiv", needMap = TRUE, testInhib = TRUE)
+createDataframe("newProject", standType ="indiv")
 
 ###################################################################################################
-#Create the dataframe.
-
-createDataframe("mTest1", standType ="indiv", needMap = TRUE, addSIR = TRUE)
-
+#Quantify growth requirement
 ###################################################################################################
-#Note that if the values are not matching up to your expected values based on your ImageJ values there are a couple of things to try.
-###################################################################################################
- #The first is to use a different critical value for the radius. The default is a 20% reducation in growth, but a 50% reduction might be better.
-createDataframe("newProject", standType ="indiv",  RADcrit = "50%",  needMap = TRUE, addSIR = TRUE)
-
-#If this doesn't work then the next thing to try is to turn off 'testInhib' in maxLik and then try with both critical values
-maxLik("newProject", maxDist = 25, standType = "indiv", needMap = TRUE, testInhib = FALSE)
-createDataframe("newProject", standType ="indiv", needMap = TRUE, addSIR = TRUE)
-createDataframe("newProject", standType ="indiv",  RADcrit = "50%",  needMap = TRUE, addSIR = TRUE)
-
-#If none of that matches then I can go back in and try out different critical values of the radius to see what matches best.
+inhibGrowPts("newProject")
